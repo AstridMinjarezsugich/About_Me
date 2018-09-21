2a277055535e2c16a7a16447d84ba62d679c7278
astrid minjarezsugich <astridminjarezsugich@gmail.com>
2018-09-20 19:59:21 -0700

    Added Top 10 List

--- js/app.js
+++ js/app.js
@@ -10,20 +10,19 @@ console.log('What is your name? : ' + userName);
 
 //Coding experience question
 var experienceQuestion = function(){
-
 var coding = prompt ('Do I have Coding experience?');
 coding = coding.toLowerCase();
 
 if (coding === 'yes' || coding === 'y'){
-     alert ('Wrong, ' + userName + ' besides messing with my myspace layouts, I have no previous coding experience.');
-     } else if(coding === 'no' || coding === 'n'){
-        alert( 'You are RIGHT, I do not have any previous experience.');
-        correctResponse++;     
-    } else {
-            alert('Please respond using yes or no or y or n.');
-         }
-         console.log('Do I have Coding Experience? : ' + coding);
-return experienceQuestion;
+           alert ('Wrong, ' + userName + ' besides messing with my myspace layouts, I have no previous coding experience.');
+    }      else if(coding === 'no' || coding === 'n'){
+           alert( 'You are RIGHT, I do not have any previous experience.');
+           correctResponse++;     
+        }  else {
+           alert('Please respond using yes or no or y or n.');
+        }
+           console.log('Do I have Coding Experience? : ' + coding);
+           return experienceQuestion;
 };
 
 // Party question
@@ -32,101 +31,92 @@ var party = prompt ('Do I like to party like it\'s 1999?');
 party = party.toLowerCase();
 
 if (party === 'yes' || party === 'y'){
-      alert ('So right about that, ' + userName + ' is all about the experience you won\'t forget!');
-      correctResponse++;  
-    } else if(party === 'no' || party === 'n'){
-        alert( 'Dammm...and I thought we were friends..');
-         } else {
+          alert ('So right about that, ' + userName + ' is all about the experience you won\'t forget!');
+          correctResponse++;  
+    }     else if(party === 'no' || party === 'n'){
+          alert( 'Dammm...and I thought we were friends..');
+        } else {
           alert('Please respond using yes or no or y or n.');
-         }
-
-     console.log('Do I like to party like it\'s 1999? : ' + party);
-return partyQuestion;
+        }
+          console.log('Do I like to party like it\'s 1999? : ' + party);
+          return partyQuestion;
 };
 
 //College question
 var collegeQuestion = function(){
-
 var college = prompt ('Did I complete college education?');
 college = college.toLowerCase();
 
 if (college === 'no' || college === 'n'){
-     alert ('Right again, ' + userName + ' not yet.');
-     correctResponse++;  
-    } else if(college === 'yes' || college === 'y'){
-        alert( 'Nope, I wish, but someday I will go back and finish');
-         } else {
-          alert('Please respond using yes or no or y or n.');
-         }
-
-     console.log('Did I complete college education? : ' + college);
-return collegeQuestion;
-}
+            alert ('Right again, ' + userName + ' not yet.');
+            correctResponse++;  
+    }       else if(college === 'yes' || college === 'y'){
+            alert( 'Nope, I wish, but someday I will go back and finish');
+        }   else {
+            alert('Please respond using yes or no or y or n.');
+        }
+            console.log('Did I complete college education? : ' + college);
+            return collegeQuestion;
+};
 
 //Middle name question
 var middleNameQuestion = function(){
-
 var middleName = prompt ('Do I have a middle name?');
 middleName = middleName.toLowerCase();
 
 if (middleName === 'yes' || middleName === 'y'){
-  alert ('That\'s right, ' + userName + ' I do have one.');
-  correctResponse++; 
-} else if(middleName === 'no' || middleName === 'n'){
-  alert( 'Oh man! here I go againg thinking you know me');
-} else {
-  alert('Please respond using yes or no or y or n.');
-}
-
-console.log('Do I have a middle name? : ' + middleName);
-return middleNameQuestion;
-}
+            alert ('That\'s right, ' + userName + ' I do have one.');
+            correctResponse++; 
+    }       else if(middleName === 'no' || middleName === 'n'){
+               alert( 'Oh man! here I go againg thinking you know me');
+        }   else {
+            alert('Please respond using yes or no or y or n.');
+        }
+            console.log('Do I have a middle name? : ' + middleName);
+            return middleNameQuestion;
+};
 
 //Life
 var lifeQuestion = function(){
-
 var life = prompt ('Do I believe in the possability of life in outerspace?');
 life = life.toLowerCase();
 
 if (life === 'yes' || life === 'y'){
-  alert ('Ohh Yeahhh, ' + userName + ' let\'s discuss the topic.');
-  correctResponse++; 
-} else if(life === 'no' || life === 'n'){
-  alert( 'No?!! you must not know much about me.');
-} else {
-  alert('Please respond using yes or no or y or n.');
-}
-
-console.log('Do I believe in the possability of life in outerspace? : ' + life);
-return lifeQuestion;
-}
+         alert ('Ohh Yeahhh, ' + userName + ' let\'s discuss the topic.');
+         correctResponse++; 
+    }    else if(life === 'no' || life === 'n'){
+         alert( 'No?!! you must not know much about me.');
+        }else {
+         alert('Please respond using yes or no or y or n.');
+        }
+         console.log('Do I believe in the possability of life in outerspace? : ' + life);
+         return lifeQuestion;
+};
 
 //number question
 var coffeeQuestion = function(){
-
 var cupsPerDay = 4;
 var cups = prompt('How many cups of coffee did I drink today?');
 
 for (var attempts = 0; attempts < 4; attempts++){
   
   if (cupsPerDay === cups){
-    alert ('Thats right');
-    correctResponse++; 
-    break;
-  } else if( cups < cupsPerDay){
-    cups = parseInt(prompt ('I needed a little bit more ' + userName));
-  } else if (cups === '0'){
-    cups = parseInt(prompt( 'I like to have at least one cup a day'));
-  } else if (cups > cupsPerDay){
-    cups = parseInt(prompt('That\'s too much for me'));
-  } else {
-    cups = parseInt(prompt('your answer must be a number'));
-  }
-
-}
-console.log('How many cups of coffee did I drink today? : ' + cups);
-return coffeeQuestion;
-}
+                 alert ('Thats right');
+                 correctResponse++; 
+                 break;
+    }            else if( cups < cupsPerDay){
+                 cups = parseInt(prompt ('I needed a little bit more ' + userName));
+        }        else if (cups === '0'){
+                 cups = parseInt(prompt( 'I like to have at least one cup a day'));
+        }        else if (cups > cupsPerDay){
+                 cups = parseInt(prompt('That\'s too much for me'));
+            }    else {
+                 cups = parseInt(prompt('your answer must be a number'));
+            }
+                }
+                 console.log('How many cups of coffee did I drink today? : ' + cups);
+                 return coffeeQuestion;
+};
 
 //Multiple choice Question
 var fruitQuestion = function(){
@@ -155,7 +145,7 @@ do{
     guessCounter--;
 } while(guessCounter > 0 && !correctAnswer);
 return fruitQuestion;
-}
+};
 
 //Quiz Score
 var quizScore = function(){
@@ -163,7 +153,7 @@ var quizScore = function(){
 console.log(correctResponse);
 alert('You answer ' + correctResponse + ' out of 7 questions correctly');
 return quizScore;
-}
+};
 
 //functions to call
 experienceQuestion();
